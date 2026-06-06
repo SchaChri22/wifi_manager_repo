fd7a8c08cd4569c4837b19fa01aedd6e440f268e
CSchaberreiter <c.schaberreiter@htlpinkafeld.at>
2026-06-06 11:00:54 +0200

    loadAllCourses

--- frontend/model.js
+++ frontend/model.js
@@ -2,17 +2,34 @@
 // model.js
 
 export function Kurse(){
-    this.Kurse = [];
+    this.kursListe = [];
 };
 
 export function Kunden(){
-    this.Kunden = [];
+    this.kunden = [];
 };
 
-export function Kunden(name, mail, payform, choosenCourse){
+export function Kunde(name, mail, payform, choosenCourse){
     this.name = name;
     this.mail = mail;
     this.payform = payform;
     this.choosenCourse = choosenCourse;
+};
+
+Kurse.prototype.loadAllCourses = async function(){
+    try {
+        const response = await fetch("http://localhost:3000/kurse");
+
+        if (!response.ok){
+            throw new Error("HTTP-Fehler: " + response.status);
+        };
+
+        const daten = await response.json();
+        console.log("Daten erhalten: ", daten);
+        this.kursListe = daten;
+    }
+    catch (error){
+            console.log("Fehler: ", error.message);
+    }
 };
 
